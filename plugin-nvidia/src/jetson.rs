b98044d3ef335e962a3d49bcd3b3911554ad89d2
rutu-sh <ruturajs10@hotmail.com>
2025-03-23 23:59:05 -0400

    fix: small fixes

--- plugin-nvidia/src/jetson.rs
+++ plugin-nvidia/src/jetson.rs
@@ -181,7 +181,7 @@ fn detect_hierarchy_modern<P: AsRef<Path>>(sys_ina: P) -> anyhow::Result<Vec<Ina
 
     /// Look for a path of the form <sensor_path>/hwmon/hwmon<id>
     fn sensor_channels_dir(sensor_path: &Path) -> anyhow::Result<PathBuf> {
-        return Ok(PathBuf.from("/sys/bus/i2c/drivers/ina3221/1-0040/hwmon/hwmon2".to_string()));
+        return Ok(PathBuf::from("/sys/bus/i2c/drivers/ina3221/1-0040/hwmon/hwmon2"));
     }
 
     fn guess_channel_unit(prefix: &Option<Match>) -> Option<PrefixedUnit> {
