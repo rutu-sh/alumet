36614548e97221c693a51f8282f6a04c45052705
rutu-sh <ruturajs10@hotmail.com>
2025-03-24 00:09:55 -0400

    fix: small fixes

--- plugin-nvidia/src/jetson.rs
+++ plugin-nvidia/src/jetson.rs
@@ -299,7 +299,7 @@ fn detect_hierarchy<P: AsRef<Path>>(
                 // Extract the prefix, suffix and channel id.
                 let (prefix, suffix) = (&groups.name("prefix"), &groups.name("suffix"));
 
-                if let Some("shunt") = prefix {
+                if prefix == "shunt" {
                     continue;
                 }
 
